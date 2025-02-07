79d38495115aba6af5ba5d2d144f5fede4cb96f8
Enrique Avila <senquevila@gmail.com>
2025-02-06 18:01:09 -0600

    primer commit

---
+++ ConsoleApp/cap3/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello\n\t\"World\"");
+Console.WriteLine(@"C:\Users\
+        Parece ser otra linea
+
+
+hi
+");
+
+
+// Comparaciones
+int x = 10;
+int y = 20;
+int z = 0;
+bool evaluacion = y > 15 || (y/z > x && z > x);
+Console.WriteLine(evaluacion);
+Console.WriteLine(5 + 4 * 2 / 2 - 1);
