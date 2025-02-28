c73ed615db9a761cec20f71ab09b508dbbd95979
Enrique Avila <senquevila@gmail.com>
2025-02-27 18:59:50 -0600

    Funciones auxiliares

--- ConsoleApp/cap-11/Tarea.cs
+++ ConsoleApp/cap-11/Tarea.cs
@@ -8,5 +8,21 @@ namespace cap_11
 {
     internal class Tarea
     {
+        public string Nombre {  get; set; }
+        public DateTime FechaLimite { get; set; }
+
+        public EstadoTarea Estado {  get; set; }
+
+
+        public enum EstadoTarea { PENDIENTE, COMPLETADO, VENCIDO };
+
+
+        public Tarea() { 
+            Nombre = string.Empty;
+            FechaLimite = DateTime.Now.AddDays(7.0);
+            Estado = EstadoTarea.PENDIENTE;
+        }
+    
     }
+
 }
